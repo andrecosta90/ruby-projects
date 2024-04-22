1716d161337488b6b9acf15a5b8a8bfaff8ae964
André Costa <andrehesilva@gmail.com>
2024-04-22 19:21:58 -0300

    Add naive fib

--- recursion/fibs.rb
+++ recursion/fibs.rb
@@ -1,1 +1,27 @@
-#fibs
+# frozen_string_literal: true
+
+# fibs
+
+def fibs_it(n)
+  return n if n <= 1
+
+  fib = 0
+  f0 = 0
+  f1 = 1
+  (2..n).each do |_|
+    fib = f0 + f1
+    f0 = f1
+    f1 = fib
+  end
+  fib
+end
+
+def fibs(n)
+  return n if n <= 1
+
+  fibs(n - 1) + fibs(n - 2)
+end
+
+(0..10).each do |i|
+  puts "#{i} :: #{fibs_it(i)} | #{fibs(i)}"
+end
